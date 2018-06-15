5057176fc382051cc39182d40c3cb07d23a5b638
ArtoLabs <artopium@gmail.com>
2018-06-15 07:10:11 -0600

    Refactored further, added methods for creating a new user. Redesigned databases to include user table.

--- steemax/axdb.py
+++ steemax/axdb.py
@@ -9,10 +9,8 @@ from steemax.db import DB
 from screenlogger.screenlogger import Msg
 
 
+
 class AXdb(DB):
-    '''Used to retrieve and store data 
-    in the steemax MySQL database
-    '''
 
 
 
@@ -25,33 +23,25 @@ class AXdb(DB):
 
 
     def first_time_setup(self):
-        '''Check to see if the the table named 
-        "axlist" is present in the database "steemax". 
-        If not make it.
-        Check to see if the the table named "axglobal" 
-        is present in the database "steemax". If not make 
-        it and initialize it.
-        '''
+        if not self.get_results("SELECT * FROM users WHERE 1;"):
+            self.commit('CREATE TABLE IF NOT EXISTS users '
+                + '(ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, '
+                + 'Account varchar(50), PrivateKey varchar(100), '
+                + 'RefreshToken varchar(400), Token varchar(400), '
+                + 'Time TIMESTAMP DEFAULT CURRENT_TIMESTAMP);')
         if not self.get_results("SELECT * FROM axlist WHERE 1;"):
             self.commit('CREATE TABLE IF NOT EXISTS axlist '
                 + '(ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, '
                 + 'Account1 varchar(50), Account2 varchar(50), '
-                + 'Key1 varchar(100), Key2 varchar(100), Token1 varchar(200), '
-                + 'Token2 varchar(200), Percentage varchar(5), '
-                + 'Ratio varchar(5), Duration varchar(5),MemoID varchar(40), '
-                + 'Status varchar(10), Time TIMESTAMP DEFAULT CURRENT_TIMESTAMP);')
-        if not self.get_results("SELECT * FROM axglobal WHERE 1;"):
-            self.commit('CREATE TABLE IF NOT EXISTS axglobal '
-                + '(ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, '
-                + 'RewardBalance varchar(50), RecentClaims varchar(50), '
-                + 'Base varchar(50), Time TIMESTAMP DEFAULT CURRENT_TIMESTAMP);')
-            self.commit('INSERT INTO axglobal (ID, RewardBalance, '
-                + 'RecentClaims, Base) VALUES ("1", "0", "0", "0");')
+                + 'Percentage varchar(5), Ratio varchar(5), '
+                + 'Duration varchar(5), MemoID varchar(100), '
+                + 'Status varchar(10), '
+                + 'Time TIMESTAMP DEFAULT CURRENT_TIMESTAMP);')
         if not self.get_results("SELECT * FROM axtrans WHERE 1;"):
             self.commit('CREATE TABLE IF NOT EXISTS axtrans '
                 + '(ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, '
                 + 'TXID varchar(50), MemoFrom varchar(20), '
-                + 'Amount varchar(20), MemoID varchar(40), '
+                + 'Amount varchar(20), MemoID varchar(100), '
                 + 'Action varchar(20), TxTime TIMESTAMP NULL, '
                 + 'DiscoveryTime TIMESTAMP NOT NULL '
                 + 'DEFAULT CURRENT_TIMESTAMP '
@@ -85,48 +75,10 @@ class AXdb(DB):
         
 
 
-    def check_reward_fund_renewal(self):
-        ''' Checks the timestamp from the 
-        database to see if the stored 
-        info is stale (older than 120 seconds)
-        '''
-        delta = datetime.now() - self.dbresults[0][3]
-        if delta.seconds > 120:
-            return True
-        else:
-            return False
-
-
-
-    def save_reward_fund(self, rb, rc, base):
-        ''' Saves the current Reward Balance, 
-        Recent Claims and the price of STEEM in the 
-        database for quicker retreival and 
-        less taxation on steem nodes.
-        '''
-        return self.commit('UPDATE axglobal SET RewardBalance = %s, '
-            + 'RecentClaims = %s, Base = %s, WHERE ID = "1";',
-            rb, rc, base)
-
-
-
-    def fetch_reward_fund(self):
-        ''' Retrives the Reward 
-        Balance, Recent Claims and 
-        the price of STEEM from the 
-        database instead of from
-        the steem node
-        '''
-        return self.get_results('SELECT RewardBalance, RecentClaims, '
-            + 'Base, Time FROM axglobal WHERE ID = "1";')
-
-
-
     def verify_memoid(self, acct0, memoid):
         '''Verify that the Memo ID entered 
-        matches the account name entered
-        This is necessary so that each account 
-        can update the invite during the barter process
+        matches the account name entered. 
+        Also determines who's who
         '''
         if not self.get_results('SELECT Account1, Account2, '
                                 + 'Status FROM axlist WHERE MemoID = %s;',
@@ -158,41 +110,27 @@ class AXdb(DB):
 
 
     def update_invite(self, percent, ratio, duration, memoid, status):
-        '''This is used during the barter process. 
-        Both accounts can update the percentage,
-        ratio and duration of the exchange until an 
-        agreement is made. This pauses the exchange
-        and puts it into a status which indicates an 
-        agreement has not been made.
-        Update the percentage, ratio and duration based 
-        on the Memo ID which is verified using
-        the verify_memoid function.
-        '''
         return self.commit('UPDATE axlist SET Percentage = %s, '
             + 'Ratio = %s, Duration = %s, Status = %s WHERE MemoID = %s;',
             percent, ratio, duration, status, memoid)
 
 
 
-    def update_status(self, status):
-        ''' Updates the status of an exchange, specifically 
-            used if a barter has been started. The status 
-            numbers represent:
+    def update_status(self, status, memoid):
+        '''
         -1 = waiting for inviter to authorize
-         0 = invite has beed sent. waiting for invitee to authorize
-         1 = exchange authorized by both parties, exchange is underway
+         0 = invite sent. waiting for invitee
+         1 = exchange authorized by both parties
          2 = inviter (account1) has offered to barter
          3 = invitee (account2) has offered to barter
          4 = exchange has been cancelled
         '''
-        return self.commit("UPDATE axlist SET Status = %s;", status)
+        return self.commit("UPDATE axlist SET Status = %s WHERE MemoID = %s;", 
+            status, memoid)
 
 
 
     def check_status (self, memoid):
-        ''' The check status method is used to determine 
-        if authorization requests are valid
-        '''
         if self.get_results("SELECT Status FROM axlist WHERE MemoID = %s;", 
                 memoid):
             return self.dbresults[0][0]
@@ -201,32 +139,21 @@ class AXdb(DB):
 
 
 
-    def accept_invite(self, acct2, memoid, key2, refreshtoken2):
-        '''The exchange is initiated when both 
-        accounts agree. If the invitee wishes to barter, 
-        this function is still invoked first.
-        then update_invite which pauses the exchange 
-        and sets the exchange status to "2" (see above)
-        Update the private posting key and set 
-        the Status to "1" which indicates
-        an agreement has been made and thus making 
-        the auto-upvote exchange active
-        '''
-        return self.commit('UPDATE axlist SET Key2 = %s, RefreshToken2 = %s, '
-            + 'Status = "0" WHERE Account2 = %s AND (MemoID = %s);',
-            key2, refreshtoken2, acct2, memoid)
+    def get_user_token (self, acct):
+        if self.get_results("SELECT PrivateKey, Token, RefreshToken "
+                            + "FROM users WHERE Account = %s;",
+                            acct):
+            return self.dbresults[0][1]
+        else:
+            return False
 
 
 
-    def add_invite (self, acct1, acct2, key1, refreshtoken1, 
-            accesstoken1, percent, ratio, duration):
+    def add_invite (self, acct1, acct2, percent, ratio, duration):
         '''Adds the initial invite to the database 
         and provides the unique Memo ID.
-        This sets the Status to "0" indicating that the invitation
-        has not been accepted nor has a barter 
-        process been started.
         '''
-        memoid = self.generate_nonce()
+        memoid = self.generate_memoid()
         if acct1 == acct2:
             self.msg.message('The same account name was '
                         + 'entered for both accounts.')
@@ -239,10 +166,10 @@ class AXdb(DB):
                         + 'made between these accounts.')
             return False
         if self.commit('INSERT INTO axlist (Account1, Account2, '
-                        + 'Key1, RefreshToken1, Token1, Percentage, '
-                        + 'Ratio, Duration, MemoID, Status) '
-                        + 'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);',
-                        acct1, acct2, key1, refreshtoken1, accesstoken1,
+                        + 'Percentage, Ratio, '
+                        + 'Duration, MemoID, Status) '
+                        + 'VALUES (%s, %s, %s, %s, %s, %s, %s);',
+                        acct1, acct2, 
                         percent, ratio, duration, memoid, -1):
             return memoid
         else:
@@ -250,17 +177,21 @@ class AXdb(DB):
 
 
 
+    def add_user (self, acct, key, refreshtoken, accesstoken):
+        return self.commit('INSERT INTO user (Account, PrivateKey, '
+                        + 'RefreshToken, Token) '
+                        + 'VALUES (%s, %s, %s, %s);',
+                        acct, key, refreshtoken, accesstoken)
+            
+
+
     def get_axlist (self):
-        ''' Gets all auto-upvote exchanges
-        '''
         self.get_results("SELECT * FROM axlist WHERE 1;")
         return self.dbresults
 
 
 
-    def generate_nonce(self, length=32):
-        '''Generates the unique Memo ID
-        '''
+    def generate_memoid(self, length=32):
         return ''.join([str(random.randint(0, 9)) for i in range(length)])
 
 
@@ -268,15 +199,8 @@ class AXdb(DB):
 # Run as main
 
 if __name__ == "__main__":
+    pass
 
-    a = AXdb("steemax", "SteemAX_pass23", "steemax")
-    m = Msg()
-
-    if a.fetch_reward_fund():
-        m.message("Balance: " + a.dbresults[0][0] + "\nClaims: " 
-                    + a.dbresults[0][1] + "\nSteem: $" + a.dbresults[0][2])
-    else:
-        m.message("No results from database while testing axdb.py")
     
 
 # EOF
