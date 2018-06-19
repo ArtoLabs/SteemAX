c039633f4af518cf5b869826556cf9d760dbaf00
ArtoLabs <artopium@gmail.com>
2018-06-19 11:19:24 -0600

    version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     python_requires='>=3.0',
     name='SteemAX',
-    version='0.3.3',
+    version='0.3.4',
     packages=['steemax'],
     license='MIT',
     long_description=open('README.txt').read(),
