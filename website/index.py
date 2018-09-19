bb67853dfca612dc9f2985dd41fb97fa81c14ab7
ArtoLabs <artopium@gmail.com>
2018-09-18 18:10:27 -0600

    Changed the index script.
    This script no longer redircts to SteemConnect but simply displays the new home page.

--- website/index.py
+++ website/index.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 from cgi import FieldStorage
 from steemax import web
-code = FieldStorage().getvalue('code')
 print ("Content-type: text/html")
-print (web.Web().login(code))
+print (web.Web().index_page())
