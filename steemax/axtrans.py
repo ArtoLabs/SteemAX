0a769f4d590388a00c65380c8adecb80bb4948b2
ArtoLabs <artopium@gmail.com>
2018-10-06 16:26:43 -0600

    Optimized exchanges
    Added a dictionary to remember account vote data while accounts are being compared to each other. This has drastically reduced the number of calls to the blockchain and has sped up exchanges 100 fold.

--- steemax/axtrans.py
+++ steemax/axtrans.py
@@ -39,12 +39,15 @@ class MemoMsg:
                + "At a ratio of {}:1 for {} days. "
                ).format(acct2, acct1,
                         invite[0], invite[1], invite[2])
+        # Verify the user has an account. If they do,
+        # send them the memo id code
         if self.db.get_user_token(acct2):
             msg += ("Please send any amount SBD to "
-                     + "@steem-ax with the following in "
-                     + "the memo: '{}:accept'. ").format(memoid)
+                    + "@steem-ax with the following in "
+                    + "the memo: '{}:accept'. ").format(memoid)
+        # Otherwise we skip that part of the message. 
         msg += ("For more info please visit "
-                 + "https://steemax.info/@{}").format(acct2)
+                + "https://steemax.info/@{}").format(acct2)
         self.returnmsg = msg
 
     def accepted_msg(self, acct, memoid):
@@ -62,10 +65,13 @@ class MemoMsg:
         """ Creates the barter message
         """
         self.reaction = reaction
+        # We must choose the proper pronun because the percentage
+        # is always taken from the inviter's (not invitee) upvote 
         if status == 2:
             pronoun = "their"
         else:
             pronoun = "your"
+        # ADd the new values to the database
         self.db.update_invite(per, ratio,
                               dur, memoid, status)
         self.returnmsg = (
@@ -85,14 +91,17 @@ class MemoMsg:
         self.reaction = "refund"
         self.returnmsg = reason
 
-    def cancel(self, canceller, memoid):
+    def cancel(self, canceller, memoid, rstatus):
         """ The cancel command can be 
         given at any time by either party
         """
-        self.reaction = "cancelled"
-        self.db.update_status(4, memoid)
-        self.returnmsg = ("@" + canceller
-                          + " has cancelled the exchange.")
+        if int(rstatus) == 4:
+            self.ignore("The exchange has already been canceled.")
+        else:
+            self.reaction = "cancelled"
+            self.db.update_status(4, memoid)
+            self.returnmsg = ("@" + canceller
+                              + " has cancelled the exchange.")
 
 
 class Reaction(MemoMsg):
@@ -101,16 +110,23 @@ class Reaction(MemoMsg):
         """ The start method grants the authority to the 
         exchange from the inviter. 
         """
+        # Only the inviter can start the exchange
         if acct1 == memofrom:
-            if rstatus < 1:
+            # if the rstatus is -1 then the exchange invite has been
+            # created in the database but not yet sent to the invitee.
+            if rstatus < 0:
                 self.invite_msg(acct1, acct2, memoid)
             else:
                 self.ignore("@{} has already".format(acct1)
                             + "started this exchange.")
-        else:
+        # If the invitee sent the memo we tell them to wait.
+        elif acct2 == memofrom:
             self.ignore("Invalid action. @{} ".format(acct1)
                         + " has not yet started this "
                         + "exchange.")
+        # If it's neither party the memo is invalid.
+        else:
+            self.ignore("Invalid Memo ID.")
 
     def accept(self, acct1, acct2, memofrom,
                rstatus, memoid):
@@ -118,12 +134,26 @@ class Reaction(MemoMsg):
         a change whenever an invite or a barter is 
         sent. 
         """
+        # The memo is from the inviter
         if acct1 == memofrom:
-            if int(rstatus) > -1 and int(rstatus) != 2:
+            # The invite is pending
+            # on the inviter to accept a barter
+            if int(rstatus) == 3:
+                # The accept message is sent
                 self.accepted_msg(acct1, memoid)
+            else:
+                self.ignore("Invalid command.")
+        # The memo is from the invitee
         elif acct2 == memofrom:
-            if int(rstatus) > -1 and int(rstatus) < 3:
+            # The invite has been initially sent
+            # (rstatus = 0) or the invite is pending on the
+            # invitee to accept a barter (rstatus = 2)
+            if int(rstatus) == 0 or int(rstatus) == 2:
+                # The accept message is sent
                 self.accepted_msg(acct2, memoid)
+            else:
+                self.ignore("Invalid command.")
+        # If the memo is from neither party it is invalid.              
         else:
             self.ignore("Invalid Memo ID.")
 
@@ -134,29 +164,53 @@ class Reaction(MemoMsg):
         the exchange.
          2 = inviter (account1) has offered to barter
          3 = invitee (account2) has offered to barter
+        When a user enter values from the web interface
+        the javascript corrects most errors. Here we
+        catch errors if the user has decided to create
+        their own memo id commands to send directly from
+        their wallet and has made a mistake.
         """
         invalid = False
         verify = axverify.AXverify()
+        # First we verify that the percentage and ratio
+        # create a valid exchange. There are some instances
+        # where the values entered, even when within the correct
+        # limits, are not possible. This is partly due to vote weight
+        # not being alloed to be lower than 1%
         if not verify.eligible_votes(
                 acct1, acct2, per, ratio, 1):
+            # The eligible_votes method returns an explanation
+            # for why the ratio and percentage were invalid
+            # usually because the ratio is too high or low
             self.ignore("Barter invalid. " + verify.response)
             return
+        # The memo is from the inviter
         if acct1 == memofrom:
-            if int(rstatus) > -1 and int(rstatus) != 2:
+            # The invitee has sent a barter offer.
+            if int(rstatus) == 3:
+                # A barter offer is sent in return
+                # to the invitee's barter offer
                 self.barter_msg("acct1 bartering", 2,
                                 acct1, per, ratio,
                                 dur, memoid, acct2)
             else:
                 invalid = True
+        # The memo is from the invitee
         elif acct2 == memofrom:
-            if int(rstatus) > -1 and int(rstatus) < 3:
+            # The invite has been initially sent
+            # (rstatus = 0) or the invite is pending on the
+            # invitee to accept a barter (rstatus = 2)
+            if int(rstatus) == 0 or int(rstatus) == 2:
+                # a barter offer is sent in return
                 self.barter_msg("acct2 bartering", 3,
                                 acct2, per, ratio,
                                 dur, memoid, acct1)
             else:
                 invalid = True
+        # The memo id matches neither party
         else:
             self.ignore("Invalid Memo ID.")
+        # The barter offer was sent by the wrong party.
         if invalid:
             self.ignore("Invalid barter. Please be sure that @"
                         + acct1 + " has started the invite. "
@@ -175,6 +229,12 @@ class AXtrans:
                             default.dbname)
         self.steem = SimpleSteem()
         self.react = Reaction()
+        self.memoid = ""
+        self.action = ""
+        self.percentage = ""
+        self.ratio = ""
+        self.duration = ""
+        self.sendto = ""
 
     def parse_memo(self, **kwargs):
         """ Parses the memo message in a transaction
@@ -188,6 +248,8 @@ class AXtrans:
         # should return false.
         except:
             return False
+        # Everything from the outside world is 
+        # filtered for security
         self.memoid = sec.filter_token(memo[0])
         if len(memo) == 2:
             self.action = sec.filter_account(memo[1])
@@ -207,7 +269,9 @@ class AXtrans:
         """ Sends the forwarded amount of SBD along
         with the reaction message
         """
+        # Split "SBD" from the amount
         r = amt.split(" ")
+        # Actually transfer it
         if self.steem.transfer_funds(to, float(r[0]),
                                      r[1], msg):
             self.msg.message(("Transaction committed. Sent {} "
@@ -230,23 +294,12 @@ class AXtrans:
                              rstatus,
                              self.memoid)
         elif self.action == "cancel":
-            self.react.cancel(self.memofrom,
-                              self.memoid)
+            self.react.cancel(self.memofrom, self.memoid, rstatus)
         elif self.action == "accept":
-            self.react.accept(acct1,
-                              acct2,
-                              self.memofrom,
-                              rstatus,
-                              self.memoid)
+            self.react.accept(acct1, acct2, self.memofrom, rstatus, self.memoid)
         elif self.action == "barter":
-            self.react.barter(acct1,
-                              acct2,
-                              self.memoid,
-                              self.memofrom,
-                              rstatus,
-                              self.percentage,
-                              self.ratio,
-                              self.duration)
+            self.react.barter(acct1, acct2, self.memoid, self.memofrom, rstatus,
+                              self.percentage, self.ratio, self.duration)
         else:
             self.react.ignore("Invalid action.")
         if self.react.reaction == "refund":
