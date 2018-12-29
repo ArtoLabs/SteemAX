3f4fe1a58dca63798cc2a4e85415acb4d66c2ea5
ArtoLabs <artopium@gmail.com>
2018-12-29 03:34:42 -0700

    Version 1.5.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     python_requires='>=3.0',
     name='SteemAX',
-    version='1.5',
+    version='1.5.2',
     packages=['steemax'],
     license='MIT',
     keywords='steemit steem upvote exchange',
